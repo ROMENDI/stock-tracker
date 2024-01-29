c2dae471d92fb62e42f25913e742d6f9750fb081
Rohan Mendiratta <97857962+ROMENDI@users.noreply.github.com>
2024-01-29 09:00:14 +0000

    added basic functionality of stock tracker

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
+
+get '/stock' do
+  symbol = params['symbol']
+  api_url = "https://cloud.iexapis.com/stable/stock/#{symbol}/quote?token=#{ENV["STOCK_KEY"]}"
+  response = HTTP.get(api_url)
+
+  if response.status.success?
+    @stock_info = response.parse(:json)
+  else
+    @error_message = "Unable to fetch stock data."
+  end
+  
+  erb(:stock)
+end 
